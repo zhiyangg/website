e526491870ab013457d05b609715e46e7cad85ad
Zhi Yang Chen <zhiyangchen003@gmail.com>
2023-12-24 21:00:59 -0500

    cleaned slate

--- public/js/resume.js
+++
@@ -1,6 +0,0 @@
-const resumeButton = document.getElementById('downloadResume');
-
-resumeButton.addEventListener('click', function () {
-    const resumeUrl = 'public/assets/resume.pdf';
-    window.open(resumeUrl, '_blank');
-});
