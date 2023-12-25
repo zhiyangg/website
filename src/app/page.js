b2cd3f401348570a2978a75a16cef07a72485a97
Zhi Yang Chen <zhiyangchen003@gmail.com>
2023-12-25 17:21:49 -0500

    completed home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from '../components/navbar'; 
+import Home from '../components/home';
 import './globals.css';
 
 const Page = () => {
   return (
     <main>
+      
+      <Home />
       <Navbar />
     </main>
   );
---
+++ src/components/home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/home.module.css'; 
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+        <Head>
+            <title>Home - Zhi Yang Chen's Portfolio</title>
+            <meta name="description" content="Welcome to my portfolio website!" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div className={styles.body}>
+            <div className={styles.content}>
+                <div className={styles.name}>
+                    <h2>Zhi Yang Chen</h2>
+                </div>
+                <div className={styles.body}>
+                    <p>Junior Software Engineer</p>
+                    <p>Computer Engineering Student at the University of Toronto</p>
+                </div>
+            </div>
+        </div>
+
+        <div className={styles.image}>
+            <Image
+                src="/img/profile.png" 
+                alt="Profile Image"
+                layout="responsive"
+                width={1} 
+                height={1} 
+            />
+        </div>
+    </main>
+  );
+}
