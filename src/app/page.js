f005625c8a7d82ac28d1f51991e399fcfa2a7e63
Zhi Yang Chen <zhiyangchen003@gmail.com>
2023-12-26 00:11:34 -0500

    working on about

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navbar from '../components/navbar'; 
 import Home from '../components/home';
+import About from '../components/about';
 import './globals.css';
 
 const Page = () => {
   return (
     <main>
-      
       <Home />
       <Navbar />
+      <About />
     </main>
   );
 };
