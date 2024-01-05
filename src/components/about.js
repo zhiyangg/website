903de22df048e7d635b6b2c08e491fe855cf28de
Zhi Yang Chen <zhiyangchen003@gmail.com>
2024-01-04 19:07:28 -0500

    finished website colour scheme

--- src/components/about.js
+++ src/components/about.js
@@ -7,6 +7,7 @@ export default function About() {
         <main>
             <div className={styles.card}>
                 <div className={styles.body}>
+                    <h2>About Me</h2>
                     <p>Hi, I'm Zhi Yang Chen, a third year Computer Engineering student at the University of Toronto. </p>
                     <p>Being immersed in the world of computer engineering, I am passionate about creating innovative software solutions </p>
                     <p>Outside academia, my world revolves around my dog and sport cars, especially the engineering marvels from Porsche </p>
