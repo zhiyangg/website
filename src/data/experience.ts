289dea7e5186efb7dbdd63c868a88c430c29271d
Zhi Yang Chen <zhiyangchen003@gmail.com>
2025-03-05 01:25:58 -0500

    updated experiences & projects, added contact links

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -4,28 +4,89 @@ const experiences = [
         company: "AMD",
         date: "May 2024 - Present",
         summary: "As an Intern at AMD, I work on various projects related to system testing, automation, and telemetry.",
-        image: "/imgs/amd.png",
+        image: "/imgs/experience/amd.png",
         responsibilities: [
             "Performed telemetry and smoke testing for new OS releases.",
             "Worked on regressions, ticket resolution, and system testing.",
             "Developed automation scripts to streamline testing workflows."
         ],
         companyLink: "https://www.amd.com",
-        technologies: "Python, Bash, PowerShell, Windows OS Testing, Automation Tools"
+        technologies: "Python, PowerShell, Windows OS Testing, Automation Tools",
+        showcase: true
     },
     {
         title: "Junior Tolling Field Technician",
-        company: "407 ETR",
+        company: "407 ETR Concession Company Limited",
         date: "May 2023 - Sep 2023",
         summary: "At 407 ETR, I contributed to the backend development of tolling systems, improving automation and data processing pipelines.",
-        image: "/imgs/407etr.png",
+        image: "/imgs/experience/407etr.png",
         responsibilities: [
-            "Developed backend scripts to process toll transactions.",
-            "Worked with SQL databases to optimize query performance.",
-            "Enhanced automation tools for better efficiency."
+            "Maintained tolling equipment through detailed inspections, diagnostics, and adjustments, securing a 98% peak performance rate and guaranteeing a 99.5% uninterrupted tolling operation",
+            "Conducted exhaustive testing of tolling equipment in a controlled laboratory environment, proactively resolving technical issues to maintain a 98% data transmission reliability rate",
+            "Assisted in the East Detolling project, decommissioning 22 legacy sites and establishing 6 new sites, resulting in a 10% improvement in tolling efficiency"
         ],
         companyLink: "https://407etr.com",
-        technologies: "Python, SQL, Shell Scripting, Automation"
+        technologies: "Python, SQL, Shell Scripting, Automation",
+        showcase: true
+    },
+    {
+        title: "Electrical Team",
+        company: "Blue Sky Solar Racing",
+        date: "Sep 2022 - Sep 2023",
+        summary: "At 407 ETR, I contributed to the backend development of tolling systems, improving automation and data processing pipelines.",
+        image: "/imgs/experience/bssr.jpeg",
+        responsibilities: [
+            "Proficiently soldered and tested circuit boards, ensuring a high degree of performance and reliability for various electronic applications, improving the team’s electrical systems",
+            "Utilized Altium Designer to design PCB layouts, create component libraries, and develop schematics for various solar car electrical systems, contributing to system functionality and advancements"
+        ],
+        companyLink: "https://www.blueskysolar.org/team",
+        technologies: "",
+        showcase: true
+    },
+    {
+        title: "Park Operations",
+        company: "City of Markham",
+        date: "May 2022 - Sep 2022",
+        summary: "At 407 ETR, I contributed to the backend development of tolling systems, improving automation and data processing pipelines.",
+        image: "/imgs/experience/cityofmarkham.png",
+        responsibilities: [
+            "Managed a schedule of regular park maintenance, coordinating with regional managers and city residents to preserve city parks and ensure cleanliness",
+            "Operated and utilized a variety of equipment, including zero-turn lawn mowers, push mowers, trimmers, and blowers, to efficiently complete tasks",
+            "Actively contributed to the development and implementation of new park maintenance procedures and policies, improving the efficiency and effectiveness of city park operations"
+        ],
+        companyLink: "https://www.markham.ca/",
+        technologies: "Python, SQL, Shell Scripting, Automation",
+        showcase: true
+    }, 
+    {
+        title: "Program Coordinator",
+        company: "Community Engagement & Family Support Centre",
+        date: "Jul 2021 - Aug 2021",
+        summary: "At 407 ETR, I contributed to the backend development of tolling systems, improving automation and data processing pipelines.",
+        image: "/imgs/experience/cefs.jpeg",
+        responsibilities: [
+            "Orchestrated daily activities and special events for a summer camp of 30 campers, encompassing online engagements, arts, crafts, and sports",
+            "Devised a versatile activity schedule, catering to the diverse preferences and requirements of the campers",
+            "Partnered with camp directors to shape and uphold the camp's overarching vision and mission"
+        ],
+        companyLink: "https://cefscentre.org/",
+        technologies: "Python, SQL, Shell Scripting, Automation",
+        showcase: true
+    }, 
+    {
+        title: "Flight Sergeant",
+        company: "Royal Canadian Air Cadets",
+        date: "Sep 2016 - Jun 2021",
+        summary: "At 407 ETR, I contributed to the backend development of tolling systems, improving automation and data processing pipelines.",
+        image: "/imgs/experience/cadets.png",
+        responsibilities: [
+            "Spearheaded a diverse group of cadets through training programs, encompassing outdoor survival skills, rigorous fitness testing, enhancing team cohesion and resilience",
+            "Cultivated robust communication channels and collaborated with Canadian Armed Forces (CAF) officers, support staff, and community volunteers, fostering a supportive learning environment",
+            "Orchestrated the planning and execution of educational workshops and leadership seminars, leading to an increase in cadet engagement and proficiency in leadership competencies."
+        ],
+        companyLink: "https://www.351silverstar.com/",
+        technologies: "Python, SQL, Shell Scripting, Automation",
+        showcase: true
     }
 ];
   
