257b9f3a4101214d1a968a770f0bfcf31c33cea1
Zhi Yang Chen <zhiyangchen003@gmail.com>
2025-03-07 20:55:30 -0500

    removed gh-pages, added typewriter effect, edited technologies in experiences

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 
-module.exports = {
-    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
-    theme: {
-      extend: {
-        scrollBehavior: ["smooth"],
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        typing: {
+          from: { width: "0" },
+          to: { width: "100%" },
+        },
+        blink: {
+          "50%": { borderColor: "transparent" },
+          "100%": { borderColor: "orange" },
+        },
+      },
+      animation: {
+        typing: "typing 3.5s steps(40, end) forwards",
+        blink: "blink 0.75s step-end infinite",
       },
     },
-    plugins: [],
-};  
+  },
+  plugins: [],
+};
