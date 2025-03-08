4253f653a94431ea6e93263e72391daf1eaa70d0
Zhi Yang Chen <zhiyangchen003@gmail.com>
2025-03-07 22:37:12 -0500

    minor landing page update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
       keyframes: {
         typing: {
           "0%": { width: "0%" }, /* start typing */
-          "50%": { width: "100%" }, /* finish typing */
+          "40%": { width: "100%" }, /* finish typing */
           "60%": { width: "100%" }, /* small pause */
           "100%": { width: "0%" }, /* delete the written text */
         },
@@ -17,7 +17,7 @@ export default {
         },
       },
       animation: {
-        typing: "typing 5s steps(80, end) infinite",
+        typing: "typing 5s steps(120, end) infinite",
         blink: "blink 0.75s step-end infinite",
       },
     },
