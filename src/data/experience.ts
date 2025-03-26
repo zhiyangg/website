047d8578b81eacab2b70e37fee4bcd9b0a10c4d9
Zhi Yang Chen <zhiyangchen003@gmail.com>
2025-03-25 21:24:21 -0400

    changed img to Image from astro for optimization

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,10 +1,17 @@
+import amdLogo from "../assets/imgs/experience/amd.png";
+import etrLogo from "../assets/imgs/experience/407etr.png";
+import bssrLogo from "../assets/imgs/experience/bssr.jpeg";
+import markhamLogo from "../assets/imgs/experience/cityofmarkham.png";
+import cefsLogo from "../assets/imgs/experience/cefs.jpeg";
+import cadetsLogo from "../assets/imgs/experience/cadets.png";
+
 const experiences = [
     {
         title: "GPU Graphics Driver Validation & Debug Engineer",
         company: "AMD",
         date: "May 2024 - Present",
         summary: "Responsible for system testing, automation, and telemetry in GPU driver validation. Work on regression testing, issue resolution, and automation to enhance efficiency in new OS releases.",
-        image: "/website/imgs/experience/amd.png",
+        image: amdLogo,
         responsibilities: [
             "Performed telemetry and smoke testing for new OS releases.",
             "Worked on regressions, ticket resolution, and system testing.",
@@ -19,7 +26,7 @@ const experiences = [
         company: "407 ETR Concession Company Limited",
         date: "May 2023 - Sep 2023",
         summary: "Maintained and tested tolling equipment, ensuring high reliability and efficiency. Assisted in major infrastructure upgrades and troubleshooting to enhance tolling operations.",
-        image: "/website/imgs/experience/407etr.png",
+        image: etrLogo,
         responsibilities: [
             "Maintained tolling equipment through detailed inspections, diagnostics, and adjustments, securing a 98% peak performance rate and guaranteeing a 99.5% uninterrupted tolling operation",
             "Conducted exhaustive testing of tolling equipment in a controlled laboratory environment, proactively resolving technical issues to maintain a 98% data transmission reliability rate",
@@ -34,7 +41,7 @@ const experiences = [
         company: "Blue Sky Solar Racing",
         date: "Sep 2022 - Sep 2023",
         summary: "Contributed to the design and testing of electrical systems for a solar-powered racing car. Worked with PCB layouts, soldering, and system optimization to enhance vehicle performance.",
-        image: "/website/imgs/experience/bssr.jpeg",
+        image: bssrLogo,
         responsibilities: [
             "Proficiently soldered and tested circuit boards, ensuring a high degree of performance and reliability for various electronic applications, improving the team’s electrical systems",
             "Utilized Altium Designer to design PCB layouts, create component libraries, and develop schematics for various solar car electrical systems, contributing to system functionality and advancements"
@@ -48,7 +55,7 @@ const experiences = [
         company: "City of Markham",
         date: "May 2022 - Sep 2022",
         summary: "Maintained and managed public parks, ensuring cleanliness and proper upkeep. Operated landscaping equipment and collaborated with city officials to improve maintenance efficiency.",
-        image: "/website/imgs/experience/cityofmarkham.png",
+        image: markhamLogo,
         responsibilities: [
             "Managed a schedule of regular park maintenance, coordinating with regional managers and city residents to preserve city parks and ensure cleanliness",
             "Operated and utilized a variety of equipment, including zero-turn lawn mowers, push mowers, trimmers, and blowers, to efficiently complete tasks",
@@ -63,7 +70,7 @@ const experiences = [
         company: "Community Engagement & Family Support Centre",
         date: "Jul 2021 - Aug 2021",
         summary: "Organized and led summer camp activities for children, developing schedules and coordinating with camp directors to provide an engaging and unforgettable experience.",
-        image: "/website/imgs/experience/cefs.jpeg",
+        image: cefsLogo,
         responsibilities: [
             "Orchestrated daily activities and special events for a summer camp of 30 campers, encompassing online engagements, arts, crafts, and sports",
             "Devised a versatile activity schedule, catering to the diverse preferences and requirements of the campers",
@@ -78,7 +85,7 @@ const experiences = [
         company: "Royal Canadian Air Cadets",
         date: "Sep 2016 - Jun 2021",
         summary: "Led cadet training programs focused on leadership, discipline, and outdoor survival skills. Worked closely with Canadian Armed Forces personnel to deliver structured learning experiences.",
-        image: "/website/imgs/experience/cadets.png",
+        image: cadetsLogo,
         responsibilities: [
             "Spearheaded a diverse group of cadets through training programs, encompassing outdoor survival skills, rigorous fitness testing, enhancing team cohesion and resilience",
             "Cultivated robust communication channels and collaborated with Canadian Armed Forces (CAF) officers, support staff, and community volunteers, fostering a supportive learning environment",
